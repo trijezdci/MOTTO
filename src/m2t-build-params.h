757314b2e4d98884b8764cc6fd75815e8d3b8b06
Benjamin Kowarsch <trijezdci@users.noreply.github.com>
2023-04-29 17:34:29 +0900

    new project prefix

---
+++ src/m2t-build-params.h
@@ -0,0 +1,24 @@
+/* m2t-build-params.h -- build parameters for m2t -- public domain file */
+
+#ifndef M2T_BUILD_PARAMS_H
+#define M2T_BUILD_PARAMS_H
+
+/* Lexical Parameters */
+
+#define M2T_MAX_IDENT_LENGTH 64
+#define M2T_MAX_NUMBER_LENGTH 32
+#define M2T_MAX_STRING_LENGTH 160
+#define M2T_MAX_COMMENT_LENGTH 4096
+#define M2T_COMMENT_NESTING_LIMIT 10
+
+/* Implemented Features */
+
+#define M2T_COROUTINES_IMPLEMENTED 0
+#define M2T_LOCAL_MODULES_IMPLEMENTED 0
+#define M2T_VARIANT_RECORDS_IMPLEMENTED 1
+#define M2T_COMMENT_PRESRVN_IMPLEMENTED 0
+
+
+#endif /* M2T_BUILD_PARAMS_H */
+
+/* END OF FILE */
